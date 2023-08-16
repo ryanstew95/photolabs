400efffd63bfea4a1029135a415a3f5dfb0b5c34
Ryan Stewart <ryanstew17@gmail.com>
2023-08-16 14:14:17 -0300

    made nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import TopicList from './components/TopicList';
 import PhotoList from './components/PhotoList';
-
+import TopNavigationBar from './components/TopNavigationBar';
 import './App.scss';
+import FavBadge from 'components/FavBadge';
 
 const App = () => {
   const [selectedTopic, setSelectedTopic] = useState(null);
@@ -14,8 +15,13 @@ const App = () => {
 
   return (
     <div className="App">
+      <div className="nav-bar">
+      <TopNavigationBar />
     <TopicList onTopicSelect={handleTopicSelect} />
+    <FavBadge/>
+ </div>
       <PhotoList selectedTopic={selectedTopic} />
+
     </div>
   );
 };

fb1a268c64bbbb4a62dae96ee24409f1662f8caf
Ryan Stewart <ryanstew17@gmail.com>
2023-08-18 22:09:29 -0300

    like icons added to photo preview

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
-
-const PhotoDetailsModal = ({ photo, onCloseModal }) => {
+import FavIcon from 'components/FavIcon';
+import { useFavContext } from 'components/FavContext';
+const PhotoDetailsModal = ({ photo, onCloseModal, id }) => {
+  const { favorites, toggleFavorite } = useFavContext();
+  const isFavorite = favorites.includes(id);
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={onCloseModal}>
@@ -13,13 +16,14 @@ const PhotoDetailsModal = ({ photo, onCloseModal }) => {
         <div className="photo-details-modal__main-photo">
           <img src={photo.urls.full} alt={`Photo by ${photo.user.name}`} />
         </div>
-        
+        <FavIcon selected={isFavorite} onClick={() => toggleFavorite(id)} />
         {/* Display similar photos */}
         <div className="photo-details-modal__similar-photos">
           {/* Map through similar photos and display them */}
           {Object.values(photo.similar_photos).map((similarPhoto) => (
             <div key={similarPhoto.id} className="photo-details-modal__similar-photo">
               <img src={similarPhoto.urls.regular} alt={`Similar Photo by ${similarPhoto.user.name}`} />
+              <FavIcon selected={isFavorite} onClick={() => toggleFavorite(similarPhoto.id)} />
             </div>
           ))}
         </div>
